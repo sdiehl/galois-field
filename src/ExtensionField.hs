a074a4971c0d41e600233c65fd4c24640dfd26ad
David Kurniadi Angdinata <david@adjoint.io>
2019-06-13 18:24:30 +0100

    Attempt implementation of field extensions

--- src/ExtensionField.hs
+++ src/ExtensionField.hs
@@ -1,53 +1,75 @@
+{-# LANGUAGE DataKinds, KindSignatures #-}
 {-# LANGUAGE DeriveGeneric, GeneralizedNewtypeDeriving #-}
 
 module ExtensionField
-  () where
+  ( ExtensionField(..)
+  ) where
 
 import Protolude
+import Data.Singletons (SingI)
 
 import GaloisField (GaloisField(..))
 import PrimeField (PrimeField(..))
 
 -- | Extension fields @GF(p^q)[X]/<f(X)>@ for @p@ prime, @q@ non-negative, and
--- @f(X)@ monic irreducible
-data ExtensionField k = EF [k]
-  deriving (Show, Generic)
-instance NFData (ExtensionField k) where
-  rnf (EF ks) = foldr seq () ks
+-- @f(X)@ monic irreducible in @GF(p)[X]@
+newtype ExtensionField k (ps :: [Nat]) = EF [k]
+  deriving (Show, Generic, NFData)
 
 -- | Extension fields are Galois fields
-instance GaloisField k => GaloisField (ExtensionField k) where
-  ch = notImplemented
-
--- | Extension fields are bounded
-instance GaloisField k => Bounded (ExtensionField k) where
-  minBound = notImplemented
-  maxBound = notImplemented
+instance (SingI ps, GaloisField k) => GaloisField (ExtensionField k ps) where
+  char = const $ char (undefined :: k) -- TODO
 
 -- | Extension fields are equatable
-instance GaloisField k => Eq (ExtensionField k) where
-  (==) = notImplemented
+instance (SingI ps, GaloisField k) => Eq (ExtensionField k ps) where
+  (==) = (. toPoly) . (==) . toPoly
 
 -- | Extension fields are fields
-instance GaloisField k => Fractional (ExtensionField k) where
-  fromRational = notImplemented
-  recip        = notImplemented
-instance GaloisField k => Num (ExtensionField k) where
-  (+)         = notImplemented
-  (*)         = notImplemented
-  abs         = notImplemented
-  signum      = notImplemented
-  fromInteger = notImplemented
-  negate      = notImplemented
+instance (SingI ps, GaloisField k) => Fractional (ExtensionField k ps) where
+  fromRational (a :% b) = fromInteger a / fromInteger b
+  {-# INLINE recip #-}
+  recip a               = case polyEEA (toPoly a) (getPoly a) of
+    ([], (f, _)) -> fromPoly f
+    _            -> panic "no multiplicative inverse."
+instance (SingI ps, GaloisField k) => Num (ExtensionField k ps) where
+  a + b         = fromPoly $ polyAdd (toPoly a) (toPoly b)
+  a * b         = fromPoly $ polyMul (toPoly a) (toPoly b)
+  abs a         = a
+  signum a      = if a == 0 then 0 else 1
+  fromInteger n = fromPoly $ let m = fromInteger n in if m == 0 then [] else [m]
+  negate        = fromPoly . map negate . toPoly
 
--- | Extension fields are integral domains
-instance GaloisField k => Integral (ExtensionField k) where
-  quotRem   = notImplemented
-  toInteger = notImplemented
-instance GaloisField k => Enum (ExtensionField k) where
-  toEnum   = notImplemented
-  fromEnum = notImplemented
-instance GaloisField k => Real (ExtensionField k) where
-  toRational = notImplemented
-instance GaloisField k => Ord (ExtensionField k) where
-  (<=) = notImplemented
+-- | Monic irreducible polynomial
+getPoly :: (SingI ps, GaloisField k) => ExtensionField k ps -> [k]
+getPoly = notImplemented -- TODO map fromInteger . fromSing
+
+-- | Conversion from polynomial
+fromPoly :: (SingI ps, GaloisField k) => [k] -> ExtensionField k ps
+fromPoly f = fix $ EF . fst . polyEEA f . getPoly
+
+-- | Conversion to polynomial
+toPoly :: (SingI ps, GaloisField k) => ExtensionField k ps -> [k]
+toPoly a@(EF f) = fst . polyEEA f $ getPoly a
+
+{-# INLINABLE polyAdd #-}
+-- | Polynomial addition
+polyAdd :: (Eq a, Num a) => [a] -> [a] -> [a]
+polyAdd [] xs         = xs
+polyAdd xs []         = xs
+polyAdd (x:xs) (y:ys) = case (x + y, polyAdd xs ys) of
+  (0, []) -> []
+  (z, zs) -> z : zs
+
+{-# INLINABLE polyMul #-}
+-- | Polynomial multiplication
+polyMul :: (Eq a, Num a) => [a] -> [a] -> [a]
+polyMul [] _      = []
+polyMul _ []      = []
+polyMul (x:xs) ys = case (xs, map (* x) ys) of
+  ([], zs) -> zs
+  (_ , zs) -> polyAdd zs $ 0 : polyMul xs ys
+
+{-# INLINABLE polyEEA #-}
+-- | Polynomial extended euclidean algorithm
+polyEEA :: [a] -> [a] -> ([a], ([a], [a]))
+polyEEA xs ps = notImplemented -- TODO
