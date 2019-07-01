09502ad25133a07662bb1b4e8ed61867563544cc
Multramate <dka316@ic.ac.uk>
2019-07-01 16:31:05 +0100

    Restructure package.yaml

--- tests/ExtensionFieldTests.hs
+++ tests/ExtensionFieldTests.hs
@@ -3,7 +3,7 @@ module ExtensionFieldTests where
 import Protolude
 
 import ExtensionField
-import PrimeField
+import Test.Tasty
 
 import GaloisFieldTests
 import PrimeFieldTests
@@ -12,87 +12,109 @@ data P11
 instance IrreducibleMonic FS2 P11 where
   split _ = x^2 + x + 1
 type FS4 = ExtensionField FS2 P11
+test_S4 :: TestTree
 test_S4 = fieldAxioms (Proxy :: Proxy FS4) "FS4"
+
 data P110
 instance IrreducibleMonic FS2 P110 where
   split _ = x^3 + x + 1
 type FS8 = ExtensionField FS2 P110
+test_S8 :: TestTree
 test_S8 = fieldAxioms (Proxy :: Proxy FS8) "FS8"
+
 data P101
 instance IrreducibleMonic FS2 P101 where
   split _ = x^3 + x^2 + 1
 type FS8' = ExtensionField FS2 P101
+test_S8' :: TestTree
 test_S8' = fieldAxioms (Proxy :: Proxy FS8') "FS8'"
 
 data P10
 instance IrreducibleMonic FS3 P10 where
   split _ = x^2 + 1
 type FS9 = ExtensionField FS3 P10
+test_S9 :: TestTree
 test_S9 = fieldAxioms (Proxy :: Proxy FS9) "FS9"
+
 data P21
 instance IrreducibleMonic FS3 P21 where
   split _ = x^2 + x - 1
 type FS9' = ExtensionField FS3 P21
+test_S9' :: TestTree
 test_S9' = fieldAxioms (Proxy :: Proxy FS9') "FS9'"
+
 data P22
 instance IrreducibleMonic FS3 P22 where
   split _ = x^2 - x - 1
 type FS9'' = ExtensionField FS3 P22
+test_S9'' :: TestTree
 test_S9'' = fieldAxioms (Proxy :: Proxy FS9'') "FS9''"
 
 instance IrreducibleMonic FM0 P10 where
   split _ = x^2 + 1
 type FL0 = ExtensionField FM0 P10
+test_L0 :: TestTree
 test_L0 = fieldAxioms (Proxy :: Proxy FL0) "FL0"
 
 instance IrreducibleMonic FM1 P10 where
   split _ = x^2 + 1
 type FL1 = ExtensionField FM1 P10
+test_L1 :: TestTree
 test_L1 = fieldAxioms (Proxy :: Proxy FL1) "FL1"
 
 instance IrreducibleMonic FM2 P10 where
   split _ = x^2 + 1
 type FL2 = ExtensionField FM2 P10
+test_L2 :: TestTree
 test_L2 = fieldAxioms (Proxy :: Proxy FL2) "FL2"
 
 instance IrreducibleMonic FM3 P10 where
   split _ = x^2 + 1
 type FL3 = ExtensionField FM3 P10
+test_L3 :: TestTree
 test_L3 = fieldAxioms (Proxy :: Proxy FL3) "FL3"
 
 instance IrreducibleMonic FM4 P10 where
   split _ = x^2 + 1
 type FL4 = ExtensionField FM4 P10
+test_L4 :: TestTree
 test_L4 = fieldAxioms (Proxy :: Proxy FL4) "FL4"
 
 instance IrreducibleMonic FVL P10 where
   split _ = x^2 + 1
 type FV2 = ExtensionField FVL P10
-test_V = fieldAxioms (Proxy :: Proxy FV2) "FV2"
+test_V2 :: TestTree
+test_V2 = fieldAxioms (Proxy :: Proxy FV2) "FV2"
 
 instance IrreducibleMonic FXL P10 where
   split _ = x^2 + 1
 type FX2 = ExtensionField FXL P10
-test_X = fieldAxioms (Proxy :: Proxy FX2) "FX2"
+test_X2 :: TestTree
+test_X2 = fieldAxioms (Proxy :: Proxy FX2) "FX2"
 
 instance IrreducibleMonic FZL P10 where
   split _ = x^2 + 1
 type FZ2 = ExtensionField FZL P10
-test_Z = fieldAxioms (Proxy :: Proxy FZ2) "FZ2"
+test_Z2 :: TestTree
+test_Z2 = fieldAxioms (Proxy :: Proxy FZ2) "FZ2"
 
-type Fq = PrimeField 21888242871839275222246405745257275088696311157297823662689037894645226208583
 data Pu
 instance IrreducibleMonic Fq Pu where
   split _ = x^2 + 1
 type Fq2 = ExtensionField Fq Pu
+test_Fq2 :: TestTree
 test_Fq2 = fieldAxioms (Proxy :: Proxy Fq2) "Fq2"
+
 data Pv
 instance IrreducibleMonic Fq2 Pv where
   split _ = x^3 - (9 + t x)
 type Fq6 = ExtensionField Fq2 Pv
+test_Fq6 :: TestTree
 test_Fq6 = fieldAxioms (Proxy :: Proxy Fq6) "Fq6"
+
 data Pw
 instance IrreducibleMonic Fq6 Pw where
   split _ = x^2 - t x
 type Fq12 = ExtensionField Fq6 Pw
+test_Fq12 :: TestTree
 test_Fq12 = fieldAxioms (Proxy :: Proxy Fq12) "Fq12"
--- tests/PrimeFieldTests.hs
+++ tests/PrimeFieldTests.hs
@@ -3,6 +3,7 @@ module PrimeFieldTests where
 import Protolude
 
 import PrimeField
+import Test.Tasty
 
 import GaloisFieldTests
 
@@ -21,26 +22,41 @@ type FVL = PrimeField 20988936657440586486151264256610222593863921
 type FXL = PrimeField 5210644015679228794060694325390955853335898483908056458352183851018372555735221
 type FZL = PrimeField 741640062627530801524787141901937474059940781097519023905821316144415759504705008092818711693940737
 
+type Fq = PrimeField 21888242871839275222246405745257275088696311157297823662689037894645226208583
+
+
+test_S2 :: TestTree
 test_S2 = fieldAxioms (Proxy :: Proxy FS2) "FS2"
 
+test_S3 :: TestTree
 test_S3 = fieldAxioms (Proxy :: Proxy FS3) "FS3"
 
+test_S5 :: TestTree
 test_S5 = fieldAxioms (Proxy :: Proxy FS5) "FS5"
 
+test_S7 :: TestTree
 test_S7 = fieldAxioms (Proxy :: Proxy FS7) "FS7"
 
+test_M0 :: TestTree
 test_M0 = fieldAxioms (Proxy :: Proxy FM0) "FM0"
 
+test_M1 :: TestTree
 test_M1 = fieldAxioms (Proxy :: Proxy FM1) "FM1"
 
+test_M2 :: TestTree
 test_M2 = fieldAxioms (Proxy :: Proxy FM2) "FM2"
 
+test_M3 :: TestTree
 test_M3 = fieldAxioms (Proxy :: Proxy FM3) "FM3"
 
+test_M4 :: TestTree
 test_M4 = fieldAxioms (Proxy :: Proxy FM4) "FM4"
 
+test_VL :: TestTree
 test_VL = fieldAxioms (Proxy :: Proxy FVL) "FVL"
 
+test_XL :: TestTree
 test_XL = fieldAxioms (Proxy :: Proxy FXL) "FXL"
 
+test_ZL :: TestTree
 test_ZL = fieldAxioms (Proxy :: Proxy FZL) "FZL"
