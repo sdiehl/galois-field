19567de8e4b512e123242a55a4d93b3f646cbbe7
Multramate <dka316@ic.ac.uk>
2019-08-28 16:50:34 +0100

    Add root of unity functions

--- src/Data/Field/Galois/Unity.hs
+++ src/Data/Field/Galois/Unity.hs
@@ -1,6 +1,7 @@
 module Data.Field.Galois.Unity
   ( RootsOfUnity
-  , Subgroup(..)
+  , rootOfUnity
+  , primitiveRootOfUnity
   ) where
 
 import Protolude
@@ -9,48 +10,54 @@ import Data.Group as G (Group(..))
 
 import Data.Field.Galois.Base as F (GaloisField(..))
 import Data.Field.Galois.Prime (Prime)
+import Text.PrettyPrint.Leijen.Text (Pretty(..))
 
 -------------------------------------------------------------------------------
--- Data types
+-- Types
 -------------------------------------------------------------------------------
-
--- | Subgroups of groups.
-class Group g => Subgroup g where
-  {-# MINIMAL def #-}
-  -- | Check if element is well-defined in subgroup.
-  def :: g -> Bool
 
 -- | @n@-th roots of unity of Galois fields.
 newtype RootsOfUnity (n :: Nat) k = U k
   deriving (Bits, Eq, Functor, Generic, NFData, Ord, Show)
 
--------------------------------------------------------------------------------
--- Group instances
--------------------------------------------------------------------------------
-
 -- Roots of unity are groups.
 instance (KnownNat n, GaloisField k) => Group (RootsOfUnity n k) where
-
   invert (U x) = U $ recip x
   {-# INLINABLE invert #-}
-
   pow (U x) n = U $ F.pow x n
   {-# INLINABLE pow #-}
 
 -- Roots of unity are monoids.
 instance (KnownNat n, GaloisField k) => Monoid (RootsOfUnity n k) where
-
   mempty = U 1
   {-# INLINABLE mempty #-}
 
 -- Roots of unity are semigroups.
 instance (KnownNat n, GaloisField k) => Semigroup (RootsOfUnity n k) where
-
   U x <> U y = U $ x * y
   {-# INLINABLE (<>) #-}
 
--- Roots of unity are subgroups.
-instance (KnownNat n, GaloisField k) => Subgroup (RootsOfUnity n k) where
+-- Roots of unity are pretty.
+instance (KnownNat n, GaloisField k) => Pretty (RootsOfUnity n k) where
+  pretty (U x) = pretty x
 
-  def = (==) mempty . flip G.pow (natVal (witness :: Prime n))
-  {-# INLINABLE def #-}
+-------------------------------------------------------------------------------
+-- Functions
+-------------------------------------------------------------------------------
+
+-- Check power of unity.
+powerOfUnity :: (Integral n, GaloisField k) => k -> n -> Bool
+powerOfUnity = ((==) 1 .) . F.pow
+{-# INLINABLE powerOfUnity #-}
+
+-- | Check root of unity.
+rootOfUnity :: forall n k . (KnownNat n, GaloisField k) => RootsOfUnity n k -> Bool
+rootOfUnity (U x) = powerOfUnity x $ natVal (witness :: Prime n)
+{-# INLINABLE rootOfUnity #-}
+
+-- | Check primitive root of unity.
+primitiveRootOfUnity :: forall n k . (KnownNat n, GaloisField k) => RootsOfUnity n k -> Bool
+primitiveRootOfUnity (U x) = powerOfUnity x n && not (any (powerOfUnity x) [1 .. n - 1])
+  where
+    n = natVal (witness :: Prime n)
+{-# INLINABLE primitiveRootOfUnity #-}
