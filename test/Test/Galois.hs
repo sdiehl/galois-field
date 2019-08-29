008a70c0db9e612442626fddb36b7133180db407
Multramate <dka316@ic.ac.uk>
2019-08-29 11:55:44 +0100

    Make tests pretty

--- test/Test/Galois.hs
+++ test/Test/Galois.hs
@@ -27,37 +27,37 @@ inverses op inv e x = op x (inv x) == e && op (inv x) x == e
 
 fieldAxioms :: forall k . GaloisField k => k -> TestTree
 fieldAxioms _ = testGroup "Field axioms"
-  [ testProperty "commutativity of addition"
-    $ commutativity ((+) :: k -> k -> k)
-  , testProperty "commutativity of multiplication"
-    $ commutativity ((*) :: k -> k -> k)
-  , testProperty "associativity of addition"
-    $ associativity ((+) :: k -> k -> k)
-  , testProperty "associativity of multiplication"
-    $ associativity ((*) :: k -> k -> k)
-  , testProperty "distributivity of multiplication over addition"
-    $ distributivity ((*) :: k -> k -> k) (+)
-  , testProperty "additive identity"
-    $ identities ((+) :: k -> k -> k) 0
-  , testProperty "multiplicative identity"
-    $ identities ((*) :: k -> k -> k) 1
-  , testProperty "multiplicative annihilation"
-    $ annihilation ((*) :: k -> k -> k) 0
-  , testProperty "additive inverses"
-    $ inverses ((+) :: k -> k -> k) negate 0
-  , testProperty "multiplicative inverses"
-    $ \x -> x /= 0 ==> inverses ((*) :: k -> k -> k) recip 1 x
+  [ testProperty "commutativity of addition" $
+    commutativity ((+) :: k -> k -> k)
+  , testProperty "commutativity of multiplication" $
+    commutativity ((*) :: k -> k -> k)
+  , testProperty "associativity of addition" $
+    associativity ((+) :: k -> k -> k)
+  , testProperty "associativity of multiplication" $
+    associativity ((*) :: k -> k -> k)
+  , testProperty "distributivity of multiplication over addition" $
+    distributivity ((*) :: k -> k -> k) (+)
+  , testProperty "additive identity" $
+    identities ((+) :: k -> k -> k) 0
+  , testProperty "multiplicative identity" $
+    identities ((*) :: k -> k -> k) 1
+  , testProperty "multiplicative annihilation" $
+    annihilation ((*) :: k -> k -> k) 0
+  , testProperty "additive inverses" $
+    inverses ((+) :: k -> k -> k) negate 0
+  , testProperty "multiplicative inverses" $
+    \x -> x /= 0 ==> inverses ((*) :: k -> k -> k) recip 1 x
   ]
 
 squareRoots :: forall k . GaloisField k => k -> TestTree
 squareRoots _ = localOption (QuickCheckMaxRatio 100)
   . localOption (QuickCheckTests 10) $ testGroup "Square roots"
-  [ testProperty "squares of square roots"
-    $ \(x :: k) -> qr x
-      ==> (((^ (2 :: Int)) <$> sr x) == Just x)
-  , testProperty "solutions of quadratic equations"
-    $ \(a :: k) (b :: k) (c :: k) -> a /= 0 && isJust (quad a b c)
-      ==> (((\x -> a * x * x + b * x + c) <$> quad a b c) == Just 0)
+  [ testProperty "squares of square roots" $
+    \(x :: k) -> qr x
+    ==> (((^ (2 :: Int)) <$> sr x) == Just x)
+  , testProperty "solutions of quadratic equations" $
+    \(a :: k) (b :: k) (c :: k) -> a /= 0 && isJust (quad a b c)
+    ==> (((\x -> a * x * x + b * x + c) <$> quad a b c) == Just 0)
   ]
 
 test :: forall k . GaloisField k => TestName -> k -> TestTree
